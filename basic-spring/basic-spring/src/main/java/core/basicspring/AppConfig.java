3f1dc52d6ffdf7c2f5115f654cdfa03eecc744ac
yujin <ohyuj2001@naver.com>
2024-01-15 19:27:46 +0900

    섹션 3. AppConfig 리팩터링

--- basic-spring/basic-spring/src/main/java/core/basicspring/AppConfig.java
+++ basic-spring/basic-spring/src/main/java/core/basicspring/AppConfig.java
@@ -1,5 +1,6 @@
 package core.basicspring;
 
+import core.basicspring.discount.DiscountPolicy;
 import core.basicspring.discount.FixDiscountPolicy;
 import core.basicspring.member.MemberService;
 import core.basicspring.member.MemberServiceImpl;
@@ -7,15 +8,25 @@ import core.basicspring.member.MemoryMemberRepository;
 import core.basicspring.order.OrderService;
 import core.basicspring.order.OrderServiceImpl;
 
+import java.nio.channels.FileChannel;
+
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
