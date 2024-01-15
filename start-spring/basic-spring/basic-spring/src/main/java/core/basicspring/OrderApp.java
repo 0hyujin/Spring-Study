92bb82950712ef5280fd2856ae920ffe2d6c7750
yujin <ohyuj2001@naver.com>
2024-01-15 17:54:22 +0900

    Test : 주문과 할인 도메인 실행과 테스트

---
+++ start-spring/basic-spring/basic-spring/src/main/java/core/basicspring/OrderApp.java
@@ -0,0 +1,24 @@
+package core.basicspring;
+
+import core.basicspring.member.Grade;
+import core.basicspring.member.Member;
+import core.basicspring.member.MemberService;
+import core.basicspring.member.MemberServiceImpl;
+import core.basicspring.order.Order;
+import core.basicspring.order.OrderService;
+import core.basicspring.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
