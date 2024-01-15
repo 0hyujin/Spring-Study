bde7f362c18d1a6bd85b144d4e6787f7b4f330d9
yujin <ohyuj2001@naver.com>
2024-01-15 18:33:04 +0900

    섹션 3. 새로운 할인 정책 개발

---
+++ basic-spring/basic-spring/src/main/java/core/basicspring/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package core.basicspring.discount;
+
+import core.basicspring.member.Grade;
+import core.basicspring.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10; //10% 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
