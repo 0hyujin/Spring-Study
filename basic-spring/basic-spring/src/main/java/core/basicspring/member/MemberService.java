15e0b5c5b633d8a54fc77e4b5e21de0a2e64870e
yujin <ohyuj2001@naver.com>
2024-01-18 15:40:46 +0900

    섹션 4. 스프링 빈 조회 - 기본

--- basic-spring/basic-spring/src/main/java/core/basicspring/member/MemberService.java
+++ basic-spring/basic-spring/src/main/java/core/basicspring/member/MemberService.java
@@ -2,7 +2,7 @@ package core.basicspring.member;
 
 public interface MemberService {
 
-    void join(Member member);
+    void join(Member member); // 새로운 회원 가입
 
-    Member findMember(Long memberId);
+    Member findMember(Long memberId); // 아이디로 회원 조회
 }
