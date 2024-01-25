cf068947454a7adf496b4b78c813f01350f90d05
yujin <ohyuj2001@naver.com>
2024-01-25 09:28:40 +0900

    7-3. 생성자 주입을 선택해라!

--- basic-spring/basic-spring/src/main/java/core/basicspring/order/OrderServiceImpl.java
+++ basic-spring/basic-spring/src/main/java/core/basicspring/order/OrderServiceImpl.java
@@ -1,35 +1,33 @@
 package core.basicspring.order;
 
 import core.basicspring.discount.DiscountPolicy;
-import core.basicspring.discount.FixDiscountPolicy;
-import core.basicspring.discount.RateDiscountPolicy;
 import core.basicspring.member.Member;
 import core.basicspring.member.MemberRepository;
-import core.basicspring.member.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ basic-spring/basic-spring/src/test/java/core/basicspring/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package core.basicspring.order;
+
+import core.basicspring.discount.FixDiscountPolicy;
+import core.basicspring.member.Grade;
+import core.basicspring.member.Member;
+import core.basicspring.member.MemberRepository;
+import core.basicspring.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
