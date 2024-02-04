3f8431c5c2ff13b89bb7c0e6508135b00a855b83
yujin <ohyuj2001@naver.com>
2024-02-04 18:51:40 +0900

    6-2. 일대다 [1:N]

--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -13,11 +13,8 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(insertable = false, updatable = false)
     private Team team;
 
     public Long getId() {
@@ -35,13 +32,4 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
 }
