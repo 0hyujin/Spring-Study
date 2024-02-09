f75853553e3e75b089ed5191654b72245cff1566
yujin <ohyuj2001@naver.com>
2024-02-09 12:23:40 +0900

    9-4. 값 타입의 비교

--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -3,6 +3,8 @@ package hellojpa;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class Address {
     private String city;
@@ -41,5 +43,20 @@ public class Address {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
