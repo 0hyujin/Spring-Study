23cc84b8924949595df95456efb691c05acb92b4
yujin <ohyuj2001@naver.com>
2024-04-26 00:17:42 +0900

    어노테이션

--- spring-core-basic/src/main/java/spring/springcorebasic/AutoAppConfig.java
+++ spring-core-basic/src/main/java/spring/springcorebasic/AutoAppConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.FilterType;
 import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
-@ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 
 }
