0f7f57998fce29c41fe4d0b4696ecbd26e520dcd
yujin <ohyuj2001@naver.com>
2024-04-27 00:05:23 +0900

    Update NetworkClient.java

--- spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
+++ spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package spring.springcorebasic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -26,5 +28,20 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    // 빈 등록을 초기화 메소드
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 등록 소멸 메소드
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
